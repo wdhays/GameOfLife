80aedb3b518fa4fcbf7ead5eb1afa388c6f55b9b
Dan Hays <daniel.hays02@gmail.com>
2018-07-18 16:35:49 -0700

    Moved all interface code to FXML and Controller

---
+++ src/main/java/com/wdhays/gol/Controller.java
@@ -0,0 +1,77 @@
+package main.java.com.wdhays.gol;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    int rowCount = 60;
+    int colCount = 60;
+    int cellWidth = 10;
+    int cellHeight = 10;
+    Image cellImageAlive= new Image(getClass().getResource("./images/cell-alive.png").toString(), true);
+    Image cellImageDead= new Image(getClass().getResource("./images/cell-dead.png").toString(), true);
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private void testButton1Action(){
+        System.out.println("Test button one was pressed!");
+    }
+    @FXML
+    private void testButton2Action(){
+        System.out.println("Test button two was pressed!");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeGridPane();
+    }
+
+    private void initializeGridPane(){
+        // Set up the rows and columns.
+        for (int i = 0; i < colCount; i++) {
+            gridPane.getColumnConstraints().add(new ColumnConstraints(cellWidth));
+        }
+        for (int i = 0; i < rowCount; i++) {
+            gridPane.getRowConstraints().add(new RowConstraints(cellHeight));
+        }
+
+        //Add an image to each cell with on OnClick mouse event.
+        for (int i = 0; i < colCount; i++) {
+            for (int j = 0; j < rowCount; j++) {
+                addImageCellsToGrid(i,j);
+            }
+        }
+    }
+
+    private void addImageCellsToGrid(int i, int j) {
+
+        ImageView cellImageView = new ImageView();
+        cellImageView.setFitWidth(cellWidth);
+        cellImageView.setFitHeight(cellHeight);
+        cellImageView.setImage(cellImageDead);
+
+        cellImageView.setOnMouseClicked(e -> {
+            System.out.printf("Mouse clicked in cell [%d, %d]%n", i, j);
+            if(cellImageView.getImage() == cellImageAlive) {
+                System.out.println("The cell is alive! Kill it.");
+                cellImageView.setImage(cellImageDead);
+            } else {
+                System.out.println("The cell is dead! Resurrect it.");
+                cellImageView.setImage(cellImageAlive);
+            }
+        });
+
+        gridPane.add(cellImageView, i, j);
+    }
+}
--- src/main/java/com/wdhays/gol/GameOfLifeApplication.java
+++ src/main/java/com/wdhays/gol/GameOfLifeApplication.java
@@ -1,12 +1,9 @@
 package main.java.com.wdhays.gol;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class GameOfLifeApplication extends Application
@@ -16,54 +13,10 @@ public class GameOfLifeApplication extends Application
         launch(args);
     }
 
-    int rowCount = 60;
-    int colCount = 60;
-    int cellWidth = 10;
-    int cellHeight = 10;
-    GridPane gridPane;
-    Button testBtn1;
-    Button testBtn2;
-    Image cellImageAlive= new Image(getClass().getResource("./images/cell-alive.png").toString(), true);
-    Image cellImageDead= new Image(getClass().getResource("./images/cell-dead.png").toString(), true);
+    @Override public void start(Stage primaryStage) throws Exception {
 
-    @Override public void start(Stage primaryStage) {
-
-        // Create and set up the gridPane.
-        gridPane = new GridPane();
-        for (int i = 0; i < colCount; i++) {
-            gridPane.getColumnConstraints().add(new ColumnConstraints(cellWidth));
-        }
-        for (int i = 0; i < rowCount; i++) {
-            gridPane.getRowConstraints().add(new RowConstraints(cellHeight));
-        }
-
-        //Add a rect that we can change the color of to each grid cell.
-        for (int i = 0; i < colCount; i++) {
-            for (int j = 0; j < rowCount; j++) {
-                addImageCellsToGrid(i,j);
-            }
-        }
-
-        // Create and set up the controls pane.
-        VBox controlsPane = new VBox();
-        testBtn1 = new Button();
-        testBtn1.setText("Button 1");
-        testBtn1.setOnAction(e -> testButton1Action());
-
-        testBtn2 = new Button();
-        testBtn2.setText("Button 2");
-        testBtn2.setOnAction(e -> testButton2Action());
-
-        controlsPane.getChildren().addAll(testBtn1, testBtn2);
-        controlsPane.setPrefWidth(200);
-        controlsPane.setPadding(new Insets(10));
-        controlsPane.setSpacing(10);
-
-        // Create the main pane and add the nested panes of the main pane.
-        BorderPane mainLayout = new BorderPane();
-        mainLayout.setCenter(gridPane);
-        mainLayout.setRight(controlsPane);
         // Add the main pane to the scene.
+        Parent mainLayout = FXMLLoader.load(getClass().getResource("GameOfLife.fxml"));
         Scene mainScene =  new Scene(mainLayout, 800, 600);
 
         // Set up the stage.
@@ -71,34 +24,5 @@ public class GameOfLifeApplication extends Application
         primaryStage.setResizable(false);
         primaryStage.setTitle("Game of Life");
         primaryStage.show();
-    }
-
-    private void addImageCellsToGrid(int i, int j) {
-
-        ImageView cellImageView = new ImageView();
-        cellImageView.setFitWidth(cellWidth);
-        cellImageView.setFitHeight(cellHeight);
-        cellImageView.setImage(cellImageDead);
-
-        cellImageView.setOnMouseClicked(e -> {
-            System.out.printf("Mouse clicked in cell [%d, %d]%n", i, j);
-            if(cellImageView.getImage() == cellImageAlive) {
-                System.out.println("The cell is alive! Kill it.");
-                cellImageView.setImage(cellImageDead);
-            } else {
-                System.out.println("The cell is dead! Resurrect it.");
-                cellImageView.setImage(cellImageAlive);
-            }
-        });
-
-        gridPane.add(cellImageView, i, j);
-    }
-
-    private void testButton1Action(){
-        System.out.println("Test button one was pressed!");
-    }
-
-    private void testButton2Action(){
-        System.out.println("Test button two was pressed!");
     }
 }

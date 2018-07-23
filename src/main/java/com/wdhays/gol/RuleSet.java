2e7845b68b5df6408f6d072389487cd75df6d7d3
Dan Hays <daniel.hays02@gmail.com>
2018-07-22 22:10:03 -0700

    Laying groundwork to implement different rulesets

--- src/main/java/com/wdhays/gol/RuleSet.java
+++ src/main/java/com/wdhays/gol/RuleSet.java
@@ -1,18 +1,25 @@
 package main.java.com.wdhays.gol;
 
 public enum RuleSet {
-    STANDARD("Standard"),
-    TEST1("Test1"),
-    TEST2("Test2");
-    //More to come...
+
+    STANDARD("Standard", "23/3"),
+    HIGHLIFE("High Life", "23/36"),
+    WALLEDCITIES("Walled Cities", "2345/45678"),
+    MAZE("Maze", "12345/3");
 
     private String label;
+    private String ruleString;
 
-    RuleSet(String label) {
+    RuleSet(String label, String ruleString) {
         this.label = label;
+        this.ruleString = ruleString;
     }
 
     public String toString() {
         return label;
+    }
+
+    public String getRuleString() {
+        return ruleString;
     }
 }
